730d53f42198efc8c907018781de88c556a952f4
Tilak Parajuli <60753682+Tilak-Parajuli@users.noreply.github.com>
2020-09-21 19:53:48 +0545

    Add files via upload

---
+++ public_private_in_c++.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+ 
+ using namespace std;
+ class player{
+     private:
+     //attributes
+     string name {"Coder"};
+     int health;
+     int xp;
+
+     public:
+     //methods
+     void talk(string text_to_say){cout<< name<<"says"<<text to say<<endl;}
+     bool is_dead();
+ };
+ int main(){
+     player tilak;  
+     //tilak.name ="Tilak"; (this is private so it will produce compilation error)
+     //cout<<tilak.health << endl; (compilation error too) //private
+     tilak.talk("Hello there"); //correct because it is in public
+     return 0;
+ }
