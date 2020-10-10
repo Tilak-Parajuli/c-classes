671046bbb55eecaf711af2d075d5ba410834d194
Tilak Parajuli <60753682+Tilak-Parajuli@users.noreply.github.com>
2020-10-10 20:21:52 +0545

    Add files via upload

---
+++ constru.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class complex{
+	private:
+		int a;
+		int b;
+	public:
+
+		complex(){
+            cout<<"Hello Guys ";
+            cout<<"Happy Dashain";
+            cout<<"\n\n";
+			
+		}
+}c1;
+
+int main(){}
