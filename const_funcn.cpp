32e9c19ab0f78425729bc3655c3987e91c8a0a96
Tilak Parajuli <60753682+Tilak-Parajuli@users.noreply.github.com>
2020-10-11 23:53:37 +0545

    Add files via upload

---
+++ const_funcn.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class Demo{
+	public:
+	char n[10]="Tilak :)";
+	void show()const{
+		cout<<"hey guys, i`m "<<n<<"\n \n";
+		}
+}d1;
+int main()
+{
+	d1.show();
+	return 0;
+}
